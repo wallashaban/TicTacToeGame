7fad718575b98abf58e65ce12234226402c5aa8c
walaa <walaa@gmail.com>
2023-12-18 21:25:18 +0200

    add new design of splash , main and available screen

--- src/tictactoegame/TicTacToeGame.java
+++ src/tictactoegame/TicTacToeGame.java
@@ -22,7 +22,7 @@ public class TicTacToeGame extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        Parent root = new SignUpBase();// FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = new SplashScreen();// FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
 
         
@@ -36,7 +36,7 @@ public class TicTacToeGame extends Application {
 //        GameRoomDesignBase teb= new GameRoomDesignBase();
 //        rootb.getChildren().add(teb);
         Scene scene = new Scene(root);
-        //stage.initStyle(StageStyle.UNDECORATED); 
+        stage.initStyle(StageStyle.UNDECORATED); 
         stage.setTitle("Text Editor app");
         stage.setScene(scene);
         stage.show();
